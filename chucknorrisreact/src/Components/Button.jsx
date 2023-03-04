af317d92a53a18f24dea6b8c272817f684fa78cf
PasqualePaliotta <114742509+PasqualePaliotta@users.noreply.github.com>
2023-03-04 11:53:48 +0100

    Button

---
+++ chucknorrisreact/src/Components/Button.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+import '/src/style/Button.css'
+
+function Button (){
+	return
+	(
+		<div className="Button">
+		Content
+		</div>
+	)
+}
+
+
+export default Button
